f7c7b169ce140a1223d758ca25dc5569b2f7ca22
Sazukii <32707420+Sazukii@users.noreply.github.com>
2018-05-16 09:13:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-'/',
-'https://www.dennisvanriet.nl/ractive_rest/ractive_data.php?size=1000'
+  '/',
+  'https://www.dennisvanriet.nl/ractive_rest/ractive_data.php?size=1000'
 ];
 
 self.addEventListener('install', function(event) {
-    // Perform install steps
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          console.log('Opened cache');
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
